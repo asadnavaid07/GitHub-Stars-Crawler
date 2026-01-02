c92eed1ef2d2c1818d3524f09ccdaec4f0cb4c62
asadnavaid07 <asadnavaid34@gmail.com>
2026-01-02 20:17:05 +0500

    updated schema

--- crawler_service.py
+++ crawler_service.py
@@ -76,7 +76,7 @@ class CrawlerService:
         return self._total_crawled
     
 
-    def _generate_cusors(self,target_count:int,batch_size:int)->Iterator[Optional[str]]:
+    def _generate_cursors(self,target_count:int,batch_size:int)->Iterator[Optional[str]]:
 
         cursor=None
         fetched=0
